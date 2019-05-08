c9f8c968ff67bd6c2b5483d6f792aa739c9b43dd
wnarifin <wnarifin@gmail.com>
2019-05-08 16:24:22 +0800

    update

--- app/quran_ms.js
+++ app/quran_ms.js
@@ -3,13 +3,28 @@
 function doCalculate() {
     khatam = document.quran_ms.khatam.value;
     khatam_total = document.quran_ms.khatam_total.value;
+    if (khatam_total > khatam) {
+        khatam_total = khatam;
+        document.quran_ms.khatam_total.value = khatam;
+    }
     ms_total = document.quran_ms.ms_total.value;
     ms_read = document.quran_ms.ms_read.value;
+    if (ms_read > ms_total) {
+        ms_read = ms_total;
+        document.quran_ms.ms_read.value = ms_total;
+    }
     day_pass = document.quran_ms.day_pass.value;
     day_total = document.quran_ms.day_total.value;
+    if (day_pass > day_total) {
+        day_pass = day_total;
+        document.quran_ms.day_pass.value = day_total;
+    }
 
     //ms_day = ((ms_total - ms_read)/(day_total - day_pass)) * khatam;
     ms_day = (ms_total*khatam - khatam_total*ms_total - ms_read) / (day_total - (day_pass - 1));
+    if (ms_day < 0) {
+        ms_day = 0;
+    }
     ms_solat = ms_day/5;
 
     document.quran_ms.ms_day.value = ms_day.toFixed(1);
